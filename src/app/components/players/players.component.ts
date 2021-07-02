cd94b0c3c9ded221a6ff983a9732178b3d8ef7e2
irenegv22 <irene.gv94@gmail.com>
2021-07-02 17:34:03 +0100

    remove, edit and add implemented in players

--- src/app/components/players/players.component.ts
+++ src/app/components/players/players.component.ts
@@ -7,16 +7,44 @@ import { Player } from 'src/app/interfaces/interfaces';
   styleUrls: ['./players.component.css']
 })
 export class PlayersComponent implements OnInit {
-  
   players: Player[] = [
     {name: "Paco", id: "4", avatar: "hola", teamId: "team1"},
     {name: "Juan", id: "5", avatar: "hola2", teamId: "team2"}
   ];
 
+  //Players inputs
+  inputName:string= "";
+  inputId: string ="";
+  inputAvatar: string="";
+  inputTeamId: string ="";
 
-  constructor() {}
+  //Boolean to know if you edit a player and if so remove the old one
+  editAndRemove:boolean = false;
+
+  // player:Player= {name:"",id:"", avatar:"", teamId:""};
+
+  constructor() { }
 
   ngOnInit(): void {
+  }
+
+  add() {
+    this.players.push({name:this.inputName, id:this.inputId, avatar:this.inputAvatar, teamId:this.inputTeamId});
+    this.inputName = "";
+    this.inputId = "";
+    this.inputAvatar = "";
+    this.inputTeamId = "";
+  };
+
+  remove(nombreJugador : string){
+    this.players = this.players.filter(player => player.name != nombreJugador)
+  };
+
+  editPlayer(player:Player){
+    this.inputName = player.name;
+    this.inputId = player.id;
+    this.inputAvatar = player.avatar;
+    this.inputTeamId = player.teamId;
   }
 
 }
